33cbfdd290905932c6e34f03505fc033eae4297a
MaksKatsenko <124710290+MaksKatsenko@users.noreply.github.com>
2023-03-20 21:11:02 +0200

    CW_12

---
+++ src/main/java/models/Main.java
@@ -0,0 +1,16 @@
+package models;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        User user = new User();
+        user.setName("John");
+        user.setLastName("Silver");
+
+        User user1 = new User().withName("Jack").withLastName("Sparrow");
+
+    }
+
+
+}
