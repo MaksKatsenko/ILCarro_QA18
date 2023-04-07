122a2214116dfe9910fe17677dd61e120a903c07
MaksKatsenko <124710290+MaksKatsenko@users.noreply.github.com>
2023-04-07 09:27:45 +0300

    CW17

--- src/test/java/tests/LoginTests.java
+++ src/test/java/tests/LoginTests.java
@@ -1,10 +1,13 @@
 package tests;
+import manager.NGListener;
 import models.User;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
+@Listeners(NGListener.class)
 public class LoginTests extends TestBase{
 
     @BeforeMethod
